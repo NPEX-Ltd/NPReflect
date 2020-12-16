2235214aec1fb0b57cdb7be24610a00898a2a88e
George Venn (Npex42) <npex42@gmail.com>
2020-12-16 13:55:57 +0000

    Added kas

--- src/np/reflect/ReflectUtils.java
+++ src/np/reflect/ReflectUtils.java
@@ -1,6 +1,10 @@
 package np.reflect;
 
+import java.lang.reflect.Field;
+import java.util.function.Consumer;
+
 import np.library.common.Logger;
+import np.library.exceptions.JuggledException;
 
 public class ReflectUtils {
 	private static Logger logger = Logger.CreateNew(ReflectUtils.class);
@@ -14,9 +18,38 @@ public class ReflectUtils {
 			logger.Warn(ilex);
 		} catch (InstantiationException inex) {
 			logger.Warn(inex);
-		} finally {
-			return null;
 		}
+		
+		return null; //Should Be Unreachable
+	}
+	public static int GetIntFieldFromObject(String name, Object object) {
+		try {
+			return GetNamedFieldFromClass(name, object.getClass()).getInt(object);
+		} catch (JuggledException ex) {
+			return 0;
+		} catch (ReflectiveOperationException refex) {
+			return 0;
+		}
+	}
+	
+	public static Field GetNamedFieldFromClass(String name, Class<?> clazz)
+	throws JuggledException {
+		try {
+			return clazz.getField(name);
+		} catch (ReflectiveOperationException ex) {
+			throw new JuggledException(ex);
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static void ConsumeAllFields(Class<?> clazz, Consumer<Field> consumer) {
+			Field[] fields = clazz.getDeclaredFields();
+			for(Field field : fields) {
+				boolean isPublic = field.isAccessible();
+				field.setAccessible(true);
+				consumer.accept(field);
+				field.setAccessible(isPublic);
+			}
 	}
 }
 
--- src/np/reflect/tests/ReflectUtilsTester.java
+++ src/np/reflect/tests/ReflectUtilsTester.java
@@ -4,14 +4,38 @@ import np.library.testing.Test;
 import np.reflect.ReflectUtils;
 import static np.library.testing.Tester.*;
 
+import java.lang.reflect.Field;
+
 public class ReflectUtilsTester {
+	
+	TestClass clazz = new TestClass();
+	
 	@Test
 	public void TestConstruction() {
 		TestClass test = ReflectUtils.Create(TestClass.class);
 		FailIfNull(test);
 	}
 	
-	private static class TestClass {
+	@Test
+	public void TestFieldInput() {
+		int numberOfDogs = ReflectUtils.GetIntFieldFromObject("dogCount", clazz);
+		System.out.println("Number Of Dogs: "+numberOfDogs);
+		FailIfNotEqual(numberOfDogs, clazz.dogCount);
+	}
+	
+	@Test
+	public void TestFieldConsumer() {
+		ReflectUtils.ConsumeAllFields(TestClass.class, this::FieldConsumer);
+	} 
+	
+	public void FieldConsumer(Field field) {
+		System.out.println(field);
+	}
+	
+	public static class TestClass {
 		public TestClass() {}
+		
+		public int dogCount = 10;
+		private byte catCount = 127;
 	}
 }
